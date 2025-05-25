647eff57e8639cb1162b613d2ecc3398e3123328
Ethan-Chew <ethanchewmh@gmail.com>
2025-05-25 15:54:53 +0800

    feat: new homepage

--- src/app/components/TermsAndPrivacy.tsx
+++ src/app/components/TermsAndPrivacy.tsx
@@ -3,27 +3,22 @@ import { MdOutlinePrivacyTip } from "react-icons/md";
 
 export default function TermsAndPrivacy() {
     return (
-        <div className="p-6 bg-dark-container rounded-lg">
-            <h2 className="text-2xl font-bold">Terms & Privacy</h2>
-            <p className="text-dark-secondary mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-
-            <div className="flex flex-col md:flex-row gap-5">
-                <div className="bg-dark-background p-4 rounded-lg flex-1 flex flex-row md:flex-col gap-5 md:gap-2 items-center md:items-start">
-                    <FaRegFileLines className="text-4xl" />
-                    <div>
-                        <p className="text-lg md:text-xl font-bold">Terms and Conditions</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                    </div>
-                </div>
-
-                <div className="bg-dark-background p-4 rounded-lg flex-1 flex flex-row md:flex-col gap-5 md:gap-2 items-center md:items-start">
-                    <MdOutlinePrivacyTip className="text-4xl" />
-                    <div>
-                        <p className="text-lg md:text-xl font-bold">Privacy Policy</p>
-                        <p>Our commitment to keeping your data secure.</p>
-                    </div>
+        <>
+            <div className="bg-dark-background p-4 rounded-lg border-2 border-dark-border flex-1 flex flex-row md:flex-col gap-5 md:gap-2 items-center md:items-start">
+                <FaRegFileLines className="text-4xl flex-shrink-0" />
+                <div className="flex-1">
+                    <p className="text-lg md:text-xl font-bold">Terms and Conditions</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
                 </div>
             </div>
-        </div>
+
+            <div className="bg-dark-background p-4 rounded-lg border-2 border-dark-border flex-1 flex flex-row md:flex-col gap-5 md:gap-2 items-center md:items-start">
+                <MdOutlinePrivacyTip className="text-4xl flex-shrink-0" />
+                <div className="flex-1">
+                    <p className="text-lg md:text-xl font-bold">Privacy Policy</p>
+                    <p>Our commitment to keeping your data secure.</p>
+                </div>
+            </div>
+        </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,50 @@ import TermsAndPrivacy from "./components/TermsAndPrivacy";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-dark-background text-white">
-      <header className="flex items-center box-border">
-        <div className="max-w-1/2 z-10 my-20 mx-10">
+    <div className="bg-dark-background text-white">
+      <header className="flex items-center">
+        <div className="md:w-1/2 z-10 my-10 md:my-32 mx-10">
           <p className="text-xl font-semibold">PayMeLah!</p>
           <p className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-dark-accent to-accent leading-[1.3] mb-5">Split shared expenses with your friends, easily.</p>
           <ScanReceiptButton />
         </div>
-        <div className="grid-background absolute h-[70vh] z-0"></div>
+        <div className="grid-background inset-0 absolute h-[70vh] z-0"></div>
       </header>
 
       <div className="p-10">
-        <h2 className="text-4xl">Splitting finances with just <span className="font-bold">three</span> steps</h2>
+        <div className="mb-20">
+          <p className="text-sm font-semibold text-dark-accent">How it Works</p>
+          <h2 className="text-4xl mb-5">Splitting finances with just <span className="font-bold">three</span> steps</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+            <div className="border-2 border-dark-border rounded-lg p-5 box-border">
+              <p className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-dark-accent to-accent mb-2">1</p>
+              <p className="text-lg font-bold">Scan Receipts Instantly</p>
+              <p className="text-dark-secondary">Use your camera to scan any receipt, restaurant, grocery, or group expense. Receipt Items are instantly itemised.</p>
+            </div>
+            <div className="border-2 border-dark-border rounded-lg p-5 box-border">
+              <p className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-dark-accent to-accent mb-2">2</p>
+              <p className="text-lg font-bold">Split Bills, The Smart Way</p>
+              <p className="text-dark-secondary">Easily split the receipt among friends or group members. Assign individual items, divide shared costs, or split evenly.</p>
+            </div>
+            <div className="border-2 border-dark-border rounded-lg p-5 box-border">
+              <p className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-dark-accent to-accent mb-2">3</p>
+              <p className="text-lg font-bold">Share the Total</p>
+              <p className="text-dark-secondary">Share the bill with your group in just a tap. Send the link via text, email, or through the app.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-row bg-dark-secondary-background px-10 p-20 items-center justify-center place-content-between border-2 border-dark-border rounded-lg gap-16">
+          <div>
+            <p className="text-sm font-semibold text-dark-accent">Our promise</p>
+            <h2 className="text-4xl">Your Privacy, Assured</h2>
+            <p className="text-dark-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+          </div>
+
+          <div className="flex flex-row items-stretch justify-center gap-5 h-full">
+            <TermsAndPrivacy />
+          </div>
+        </div>
       </div>
     </div>
   );
